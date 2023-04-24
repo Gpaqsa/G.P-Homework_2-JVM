a3bf05e747205b654e0e8bae8b82e67e76ea70b9
Gpaqsa <giorgi.paksashvili.2@btu.edu.ge>
2023-04-24 20:06:38 +0400

    The End

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,12 @@ public class Main {
             System.out.println("Sum of numbers in the array: " + sum);
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
-            e.printStackTrace();
+
         } catch (InvalidNumberException e) {
             System.out.println(e.getMessage());
-            e.printStackTrace();
+
         } catch (Exception e) {
             System.out.println("Unknown exception");
-            e.printStackTrace();
         }
     }
 }
